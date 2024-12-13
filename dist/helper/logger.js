e611cc8b5f0ee61031e9c0829f56078b7b0b57d3
ikhsanmajid <ikhsanmajid@gmail.com>
2024-12-13 23:24:50 +0700

    edit logs folder

--- dist/helper/logger.js
+++ dist/helper/logger.js
@@ -9,8 +9,8 @@ const logger = winston_1.default.createLogger({
     format: winston_1.default.format.combine(winston_1.default.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston_1.default.format.errors({ stack: true }), winston_1.default.format.splat(), winston_1.default.format.json()),
     defaultMeta: { service: 'user-service' },
     transports: [
-        new winston_1.default.transports.File({ filename: 'backend/logs/error.log', level: 'error' }),
-        new winston_1.default.transports.File({ filename: 'backend/logs/combined.log' }),
+        new winston_1.default.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston_1.default.transports.File({ filename: 'logs/combined.log' }),
     ],
 });
 exports.default = logger;
--- helper/logger.ts
+++ helper/logger.ts
@@ -10,8 +10,8 @@ const logger = winston.createLogger({
     ),
     defaultMeta: { service: 'user-service' },
     transports: [
-        new winston.transports.File({ filename: 'backend/logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'backend/logs/combined.log' }),
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
     ],
 });
 
