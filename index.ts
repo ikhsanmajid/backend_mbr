2a998d0576f67db4a7f425d703583acef531f9e5
ikhsanmajid <ikhsanmajid@gmail.com>
2024-12-14 12:23:25 +0700

    add index

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(cors(corsOpttion))
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("")
+    res.send("It Works!")
 })
 
 app.get("/time", (req: Request, res: Response) => {
